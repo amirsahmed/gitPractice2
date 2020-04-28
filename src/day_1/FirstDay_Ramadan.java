867ab48a3631d355b08acdc3d23d7034d3201849
amirsahmed <amirsahmed@outlook.com>
2020-04-28 15:40:00 -0400

    add HappyBirthday.java

--- src/day_1/FirstDay_Ramadan.java
+++ src/day_1/FirstDay_Ramadan.java
@@ -4,6 +4,9 @@ public class FirstDay_Ramadan {
     public static void main(String[] args) {
         System.out.println("Today is First Day of Ramadan");
 
+        //4th day of Ramadan
+
+
 
     }
 }
